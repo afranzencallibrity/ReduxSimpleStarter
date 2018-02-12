5b669855b4c0ec746ff9a532099e4ef633e87ac8
afranzencallibrity <afranzen@callibrity.com>
2018-02-12 15:57:28 -0500

    First commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+// reducer is a function that returns a piece of the application state
+// reducer only cares about value of state
+
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'The Dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1}
+    ]
+}
+
